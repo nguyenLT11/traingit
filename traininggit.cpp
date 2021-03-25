0cd3e84b8ef3683021ab397787d07d022ad3bd4f
NguyenLT21 <nguyenlthoe@gmail.com>
2021-03-26 00:36:24 +0700

    first commit

---
+++ traininggit.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    cout<<"Hello World\n";
+    cout<<"My name is Nguyen";
+    for(int i = 1; i < 100; i++){
+        i+=10;
+    }
+    return 0;
+}
